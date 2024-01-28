50cf9648c4e344bbeb57169112b2937e5ad397b0
yam-k <yam.k.o212@gmail.com>
2024-01-29 00:02:39 +0900

    setup.ps1にskk-jisyoのコピーを追加

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,9 @@
 md $HOME\.emacs.d
+
 sudo New-Item -ItemType SymbolicLink -Path $HOME\.emacs.d\init.el -Target $PSScriptRoot\sources\.emacs.d\init.el
+
 sudo New-Item -ItemType SymbolicLink -Path $HOME\.emacs.d\emacs.org -Target $PSScriptRoot\sources\.emacs.d\emacs.org
+
+if (!(Test-Path "$HOME\.emacs.d\skk-jisyo")) {
+    Copy-Item -Recurse "$PSScriptRoot\sources\.emacs.d\skk-jisyo" "$HOME\.emacs.d\skk-jisyo"
+}
